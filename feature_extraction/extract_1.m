55e1b1d4ee73068f03bb48be3c0e75d8a8ac60b9
Samir Moussa <sm28g11@soton.ac.uk>
2015-02-24 17:42:13 +0000

    added acceleration and deceleration + code improvements

--- feature_extraction/extract_1.m
+++ feature_extraction/extract_1.m
@@ -1,51 +1,52 @@
 %% load single driver and trip for visualisation
 driver_num = 100;
-trip_num = 5;
+trip_num = 25;
 data = csvread(['sample_data/' num2str(driver_num) ...
     '/' num2str(trip_num) '.csv'], 1, 0);
 
-%% speed and time features
-% (1) average speed (including stationary periods)
-% (2) maximum speed
-% (3) minimum speed always 0?
-% (4) total time spent stationary
-% (5) average time spent stationary
+%% extract features
 
 time = length(data);
 distance = 0;
-max_diff = 0;
-min_diff = Inf;
 stops = 0;
 
+D = zeros(time, 1);
 for i = 2:time
-    diff = sqrt(sum((data(i,:) - data(i-1,:)) .^ 2));
-    distance = distance + diff;
-    
-    if (diff > max_diff)
-        max_diff = diff;
-    end
-    if (diff < min_diff)
-        min_diff = diff;
-    end
-    if (diff == 0) % stationary
-        stops = stops + 1;
-    end
+    diffs(i-1) = sqrt(sum((data(i,:) - data(i-1,:)) .^ 2));
 end
 
 % m/s to mph
 m = 2.2369362920544;
+speed = diffs ./ m;
 
-avg_speed = (distance/time) / m;
-max_speed = max_diff / m;
-min_speed = min_diff / m;
+% speed
+avg_speed = mean(speed);
+max_speed = max(diffs);
+min_speed = min(diffs);
 
-stationary = stops %/ 3600 % in hrs
-percent_stationary = stops / time
+% stationary periods
+stationary = stops; %/ 3600 % in hrs
+percent_stationary = stops / time; %*100%
 
+% acceleration
+grad = gradient(speed);
+acc = (grad > 0) .* grad;
+max_acc = max(acc);
+min_acc = min(acc);
+avg_acc = mean(acc);
 
+% deceleration
+dec = abs((grad < 0) .* grad);
+max_dec = max(dec);
+min_dec = min(dec);
+avg_dec = mean(dec);
 
-
-
+% plot speed and acceleration over time
+speed = diffs ./ m;
+plot(speed);
+hold on;
+plot(grad);
+hold off;
 
 
 
