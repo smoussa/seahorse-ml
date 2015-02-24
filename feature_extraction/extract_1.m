fb1d2b9bf11a1a8ae3475be18fe9665f628cfae4
Samir Moussa <sm28g11@soton.ac.uk>
2015-02-24 22:48:17 +0000

    updated features and section to export to csv

--- feature_extraction/extract_1.m
+++ feature_extraction/extract_1.m
@@ -1,14 +1,14 @@
 %% load single driver and trip for visualisation
-driver_num = 100;
-trip_num = 11;
+
+driver_num = 122;
+trip_num = 24;
 data = csvread(['sample_data/' num2str(driver_num) ...
     '/' num2str(trip_num) '.csv'], 1, 0);
+
 
 %% extract features
 
 time = length(data);
-distance = 0;
-stops = 0;
 
 D = zeros(time, 1);
 for i = 2:time
@@ -16,27 +16,29 @@ for i = 2:time
 end
 
 % m/s to mph
-m = 2.2369362920544;
-speed = D ./ m;
+% m = 2.2369362920544;
+% speed = D ./ m
+speed = D;
 
 % speed
 avg_speed = mean(speed);
-max_speed = max(D);
-min_speed = min(D);
+max_speed = max(speed);
+min_speed = min(speed);
 
 % periods of very fast or very slow
 fast_spd_th = 20;
 slow_spd_th = 4;
 fast = D > fast_spd_th;
 slow = D < slow_spd_th;
-time_speeding = sum(fast);
-time_slowly = sum(slow);
-percent_fast = time_speeding / time;
-percent_slow = time_slowly / time;
+time_fast = sum(fast);
+time_slow = sum(slow);
+percent_fast = time_fast / time;
+percent_slow = time_slow / time;
 
 % stationary periods
-stationary = stops; %/ 3600 % in hrs
-percent_stationary = stops / time; %*100%
+stationary = D < 1;
+stops = sum(stationary); %/ 3600 % in hrs
+percent_stop = stops / time; %*100%
 
 % acceleration
 grad = gradient(speed);
@@ -50,6 +52,7 @@ dec = abs((grad < 0) .* grad);
 max_dec = max(dec);
 min_dec = min(dec);
 avg_dec = mean(dec);
+
 
 %% plot graphs
 
@@ -70,10 +73,27 @@ ylabel('Y');
 % highlight significant speeds (or slow speeds) on trip path
 plot(data(fast,1), data(fast,2), 'o');
 plot(data(slow,1), data(slow,2), 'o');
+plot(data(stationary,1), data(stationary,2), 'ko');
 
 hold off;
 
 
+%% export to csv
+% FEATURES:
+% [ driver, time, avg_speed, max_speed, min_speed, time_fast,
+% time_slow, percent_fast, precent_slow, stops, percent_stop, avg_acc,
+% max_acc, min_acc, avg_dec, max_dec, min_dec ]
+
+features = [driver_num time avg_speed max_speed min_speed time_fast ...
+    time_slow percent_fast percent_slow stops percent_stop avg_acc ...
+    max_acc min_acc avg_dec max_dec min_dec];
+
+dlmwrite(['feature_data/' num2str(driver_num) '.csv'], ...
+    ['driver,time,avg_speed,max_speed,min_speed,time_fast,' ...
+    'time_slow, percent_fast, precent_slow, stops, percent_stop,' ...
+    'avg_acc, max_acc, min_acc, avg_dec, max_dec, min_dec']);
+dlmwrite(['feature_data/' num2str(driver_num) '.csv'], features, ...
+    '-append');
 
 
 
