03f45a42248c2947695609a04b210c8be5e0b1e0
Samir Moussa <sm28g11@soton.ac.uk>
2015-03-05 16:29:15 +0000

    testing out diff param values

--- deep_learning/build_dbn.m
+++ deep_learning/build_dbn.m
@@ -1,4 +1,4 @@
-function [probs] = build_dbn(driver_num, dbn, opts)
+function [extremes] = build_dbn(driver_num, dbn, opts)
 
 	% fetch driver data
 	src = ['./feature_data/' num2str(driver_num) '.csv']
@@ -6,7 +6,8 @@ function [probs] = build_dbn(driver_num, dbn, opts)
 	n = length(raw);
 
 	% apply pca
-	data = apply_pca(raw);
+	% data = apply_pca(raw);
+	data = raw;
 
 	% normalise between 0 and 1 (for deep learning toolbox)
 	for i = 1:size(data,2)
@@ -34,8 +35,8 @@ function [probs] = build_dbn(driver_num, dbn, opts)
 	nn = nntrain(nn, train_x, ones(length(train_x), 1), opts);
 
 	% predict
-	probs = nnpredict(nn, train_x)
-	norm_trips = abs((probs - mean(probs)) / std(probs))
+	probs = nnpredict(nn, train_x);
+	norm_trips = abs((probs - mean(probs)) / std(probs));
 	num_extremes = length(find(norm_trips > 1))
 	extremes = find(norm_trips > 1)
 
--- deep_learning/test.m
+++ deep_learning/test.m
@@ -2,20 +2,34 @@ close all
 clear all
 format long
 
-dbn.sizes = [100 100 100];
-opts.numepochs = 10;
-opts.batchsize = 40;
+dbn.sizes = [300 300 300];
+opts.numepochs = 5;
+opts.batchsize = 10;
 opts.momentum  = 0;
 opts.alpha     = 1;
 
+% compare false and true trips
+extremes = build_dbn(106, dbn, opts);
+s = 2;
+t = setdiff(1:200,extremes);
+view_trips(105, extremes(randperm(length(extremes),s)), true);  % plot some false trips
+view_trips(105, t(randperm(length(t),s)), true);                % plot some true trips
+
+
+
+
+
+
+
+
+
+
+%%
+
 % predict across drivers
-for d = 100:100
-    build_dbn(d, dbn, opts);
-end
-
-
-
-
+% for d = 105:105
+%     build_dbn(d, dbn, opts);
+% end
 
 
 %% test with other drivers
