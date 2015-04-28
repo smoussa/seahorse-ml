96583f29567c75ebbe750b1be2e85898997b9df4
Samir Moussa <sm28g11@soton.ac.uk>
2015-04-28 12:07:48 +0100

    add multiple iteration to DBN

--- deep_learning/predict_trips.m
+++ deep_learning/predict_trips.m
@@ -2,20 +2,34 @@ close all
 clear all
 format long
 
-dbn.sizes = [300 300 300];
+% initialise parameters of dbn
+dbn.sizes = [250 250 250];
 opts.numepochs = 5;
 opts.batchsize = 10;    % large batches are more strict
 opts.momentum  = 0;
 opts.alpha     = 1;
 
-% compare false and true trips
-extremes = build_dbn(106, dbn, opts);
+driver_num = 105;
+
+% frequency of commonly false trips
+F = zeros(200,1);
+for i = 1:30
+	extremes = apply_dbn(driver_num, dbn, opts);
+	F(extremes) = F(extremes) + 1;
+end
+
+% find trips that come up false at least f times;
+f = 20;
+common = find(F >= f);
+[common F(common)]
+
+disp(['Found '  num2str(numel(common)) ' frequently false trips'])
 
 % plot
 s = 6;
-t = setdiff(1:200,extremes);
-view_trips(105, extremes(randperm(length(extremes),s)), true);  % plot some false trips
-view_trips(105, t(randperm(length(t),s)), true);                % plot some true trips
+true_trips = setdiff(1:200,common);
+view_trips(driver_num, common(randperm(length(common),s)), true);  	% plot some false trips (Fig 1)
+view_trips(driver_num, true_trips(randperm(length(true_trips),s)), true);	% plot some true trips (Fig 2)
 
 
 
