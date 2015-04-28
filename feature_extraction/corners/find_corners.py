3511ec75d8d020f1a26dd175da1255d2baa2303b
(Alexander) Joe Ally <ajally1@gmail.com>
2015-04-28 21:37:28 +0100

    Now extracts features from the corners

--- feature_extraction/corners/find_corners.py
+++ feature_extraction/corners/find_corners.py
@@ -31,7 +31,12 @@ def smooth(arr, n=75):
     ysm = np.fft.irfft(ydft)
     return np.vstack((xsm, ysm)).T
 
-def find_corners(velocity, dist=None):
+
+def compute_scalar(a):
+    a_complex = a[:,0] + a[:,1]*1j
+    return np.absolute(a_complex)
+
+def identify_corners(velocity, dist=None):
     """
     Calculates where the corners are from a sequence of velocities
     Arguments:
@@ -41,13 +46,13 @@ def find_corners(velocity, dist=None):
         sequences of indices in a list. Each sequence corresponds to a corner where the indices are positions from the original trip array
     """
     if dist is None:
-        v_complex = velocity[:,0] + velocity[:,1]*1j 
-        dist = np.absolute(v_complex)
+        dist = compute_scalar(velocity)
     res = []
+    angles = []
     i = 0
     while i < len(velocity) - 1:
         if angle_between(velocity[i], velocity[i+1]) > np.radians(5):
-            minires = [i]
+            start = i
             start_vec = velocity[i]
             prev_corner_angle = 0
             currdist = dist[i]
@@ -59,44 +64,49 @@ def find_corners(velocity, dist=None):
                 if d < 1:
                     #if the distance from last is small it's probably noise
                     continue
-                minires.append(i+j)
                 currdist += d
                 corner_angle = angle_between(start_vec, v)
-                print(np.degrees(corner_angle))
                 if currdist > 70 or corner_angle <= prev_corner_angle + np.radians(1):
                     if corner_angle > np.radians(45):
                         i = i + j
-                        res.append(minires)
+                        res.append([start, i])
+                        angles.append(corner_angle)
                         break
                     else:
                         break
                 prev_corner_angle = corner_angle
-            print()
         i += 1
-    return res
+    return (res, angles)
 
+def corners_features(velocity, dists=None):
+    if dists is None:
+        dists = compute_scalar(velocity)
+    corners, angles = identify_corners(velocity, dists)
+    if len(corners) == 0:
+        return np.array([0., 0., 0., 0., 0.])
+    corner_speeds = [sum(dists[range(c1, c2+1)])/(c1 - c2) for c1, c2 in corners]
+    max_speed = np.max(corner_speeds)
+    min_speed = np.min(corner_speeds)
+    mean_speed = np.mean(corner_speeds)
+    return np.array([float(len(corners)), np.mean(angles), max_speed, min_speed, mean_speed])
 
 def main():
     trips = read_trips(sys.argv[1])
-    t_complex = [t[:,0] + t[:,1]*1j for t in trips]
     jag_velocities = [grad2d(trip) for trip in trips]
     velocities = [smooth_low(smooth(v)) for v in jag_velocities]
-    v_complex = [v[:,0] + v[:,1]*1j for v in velocities]
-    speeds = [np.absolute(v) for v in v_complex]
+    speeds = [compute_scalar(v) for v in velocities]
+    features = np.array([corners_features(v, s) for v, s in zip(velocities, speeds)])
+    print(features)
     n = random.randint(0, 199)
-    n = 29
     print("Trip number is:", n)
-    corners = find_corners(velocities[n])
+    (corners, angles) = identify_corners(velocities[n])
     trip = trips[n]
     plt.scatter(trips[n][:,0], trips[n][:,1])#, c=speeds[n])
-    for corner in corners:
-        t = trip[corner]
+    for start, end in corners:
+        t = trip[list(range(start, end+1))]
         plt.plot(t[:,0], t[:,1], 'r', linewidth=4.0)
     plt.axis('equal')
     plt.show()
-
-
-
 
 if __name__ == '__main__':
     main()
