90223252b36d58dcdba64cafb5f93f65c740b7d3
Samir Moussa <sm28g11@soton.ac.uk>
2015-04-30 14:37:24 +0100

    updated predict trip

--- deep_learning/predict_trips.m
+++ deep_learning/predict_trips.m
@@ -5,7 +5,8 @@ format long
 %{
 	HOW TO RUN:
 	(1) Add this directory and libraries directory to MATLAB path
-	(2) run this file.
+	(2) Run extract_all.m to extract feature data
+	(2) Run predict_trips.m to predict on all extracted drivers
 %}
 
 % initialise parameters of dbn
@@ -15,81 +16,74 @@ opts.batchsize = 50;    % large batches are more strict
 opts.momentum  = 0;
 opts.alpha     = 1;
 
-driver_num = 109;
+fname = 'submission_file.csv';
+fid = fopen(fname, 'w+');
+fprintf(fid, '%s\n', 'driver_trip,prob');
 
 timestart = tic;
 
+for d = 100:101
+
+	%{
+		Run DBN
+	%}
+
+	% frequency of commonly false trips (SERIAL)
+	% F = zeros(200,1);
+	% iterations = 30;
+	% for i = 1:iterations
+	% 	extremes = apply_dbn(d, dbn, opts);
+	% 	F(extremes) = F(extremes) + 1;
+	% end
+
+	% frequency of commonly false trips (PARALLEL)
+	F = zeros(200,1);
+	iterations = 4;
+	W = cell(iterations,1);
+	parfor i = 1:iterations
+		extremes = apply_dbn(d, dbn, opts);
+		W{i} = extremes;
+	end
+	% sum up workers' results
+	for i = 1:size(W,1)
+		F(W{i}) = F(W{i}) + 1;
+	end
+
+	
 
 
 
 
+	%{
+		Save trip data
+	%}
 
-% frequency of commonly false trips (SERIAL)
-% F = zeros(200,1);
-% iterations = 30;
-% for i = 1:iterations
-% 	extremes = apply_dbn(driver_num, dbn, opts);
-% 	F(extremes) = F(extremes) + 1;
-% end
+	% find trips that come up false at least f times;
+	f = fix(iterations / 2);
+	falsy = (F >= f);
+	% [falsy F(falsy)]
 
+	disp(['Driver: ' num2str(d) ' -> Found '  num2str(numel(falsy)) ' false trips.'])
 
+	driver_num = num2str(d);
+	for t = 1:200
+		fprintf(fid, '%s\n', [driver_num '_' num2str(t) ',' num2str(falsy(t))]);
+	end
+	
 
+	%{
+		Plot
+	%}
 
-
-% frequency of commonly false trips (PARALLEL)
-F = zeros(200,1);
-iterations = 100;
-W = cell(iterations,1);
-parfor i = 1:iterations
-	extremes = apply_dbn(driver_num, dbn, opts);
-	W{i} = extremes;
+	% plot
+	% s = 6;
+	% true_trips = setdiff(1:200,falsy);
+	% view_trips(d, falsy(randperm(length(falsy),s)), true);  	% plot some false trips (Fig 1)
+	% view_trips(d, true_trips(randperm(length(true_trips),s)), true);	% plot some true trips (Fig 2)
 end
-% sum up workers' results
-for i = 1:size(W,1)
-	F(W{i}) = F(W{i}) + 1;
-end
 
-
-
-
-
-
-
-% find trips that come up false at least f times;
-f = 50;
-common = find(F >= f);
-[common F(common)]
-
-disp(['Found '  num2str(numel(common)) ' frequently false trips'])
 toc(timestart)
-
-% plot
-% s = 6;
-% true_trips = setdiff(1:200,common);
-% view_trips(driver_num, common(randperm(length(common),s)), true);  	% plot some false trips (Fig 1)
-% view_trips(driver_num, true_trips(randperm(length(true_trips),s)), true);	% plot some true trips (Fig 2)
-
-
-
-
-
-
-
-
-
-
-%%
-
-% predict across drivers
-% for d = 105:105
-%     build_dbn(d, dbn, opts);
-% end
-
-
-%% test with other drivers
-% get test driver's trips
-% test_driver = 109;
-% test_x = csvread(['feature_data/' num2str(test_driver) '.csv'], 1, 0);
+fclose(fid);
 
 
 
