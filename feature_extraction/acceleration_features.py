abbfa980f2467bcb4dc56b2720269916cac3c673
(Alexander) Joe Ally <ajally1@gmail.com>
2015-05-05 16:25:46 +0100

    updated combine features shiz

--- feature_extraction/acceleration_features.py
+++ feature_extraction/acceleration_features.py
@@ -2,6 +2,7 @@ import numpy as np
 from read_data import read_trips
 import sys
 import scipy as sp
+from scipy.signal import savgol_filter
 
 def smooth_low(arr):
     arr = np.array(arr, copy=True)
@@ -21,6 +22,12 @@ def smooth(arr, n=75):
     ysm = np.fft.irfft(ydft)
     return np.vstack((xsm, ysm)).T
 
+def smooth2(arr):
+    xf = savgol_filter(arr[:,0], 5, 2)
+    yf = savgol_filter(arr[:,1], 5, 2)
+    return np.vstack((xf, yf)).T
+
+
 def grad2d(arr):
     return np.vstack((np.gradient(arr[:,0]), np.gradient(arr[:,1]))).T
 
@@ -30,11 +37,19 @@ def compute_scalar(a):
 
 def compute_velocity(position):
     jag_velocity = grad2d(position)
-    velocity = smooth_low(smooth(jag_velocity))
+    #velocity = smooth_low(smooth(jag_velocity))
+    velocity = smooth2(jag_velocity)
     return velocity
 
 def compute_acceleration(velocity):
     return grad2d(velocity)
+
+def n_step_diff(arr, n=2):
+    res = np.zeros(arr.shape)
+    for i, a in enumerate(arr):
+        j = i - n
+        res[i] = a - arr[max(j, 0)]
+    return res
     
 def acceleration_features(velocity, angle_dt, cos_dt, speed=None):
     if speed is None:
@@ -54,11 +69,12 @@ def acceleration_features(velocity, angle_dt, cos_dt, speed=None):
     slow = speed < slow_speed
     time_slow = np.sum(slow)
 
-    speed_bins = np.hstack((np.linspace(0, 40, 20), np.inf))
+    n_bins = 100 
+    speed_bins = np.hstack((np.linspace(0, 40, n_bins), np.inf))
     speed_hist, outside = np.histogram(speed, bins=speed_bins)
     n_speed_hist = speed_hist/total_time
 
-    four_speed_bins = np.hstack((np.linspace(0, 40, 20), np.inf))
+    four_speed_bins = np.hstack((np.linspace(0, 40, n_bins), np.inf))
     four_speed_hist, outside = np.histogram(four_speed, bins=four_speed_bins)
     n_four_speed_hist = four_speed_hist/total_time
     
@@ -69,23 +85,28 @@ def acceleration_features(velocity, angle_dt, cos_dt, speed=None):
     stops = np.sum(stationary)
     fraction_stationary = stops/total_time
 
-    acc_bins = np.hstack((-np.inf, np.linspace(-10, 10, 20), np.inf))
+    acc_bins = np.hstack((-np.inf, np.linspace(-10, 10, n_bins), np.inf))
     scalar_acc = np.gradient(speed)
     acc_hist, bins = np.histogram(scalar_acc, bins=acc_bins)
     n_acc_hist = acc_hist/total_time
 
+    acc2_bins = np.hstack((-np.inf, np.linspace(-5, 5, n_bins), np.inf))
+    scalar_acc2 = np.gradient(scalar_acc)
+    acc2_hist, bins = np.histogram(scalar_acc2, bins=acc2_bins)
+    n_acc2_hist = acc2_hist/total_time
+
     angle_dt_speed = speed * angle_dt
-    angle_bins = np.hstack((-np.inf, np.linspace(-50, 50, 20), np.inf))
+    angle_bins = np.hstack((-np.inf, np.linspace(-50, 50, n_bins), np.inf))
     angle_hist, b = np.histogram(angle_dt_speed, bins=angle_bins)
     n_angle_hist = angle_hist/total_time
 
     angle_dt_acc = scalar_acc*angle_dt
-    angle_bins = np.hstack((-np.inf, np.linspace(-50, 50, 20), np.inf))
+    angle_bins = np.hstack((-np.inf, np.linspace(-50, 50, n_bins), np.inf))
     acc_angle_hist, b = np.histogram(angle_dt_acc, bins=angle_bins)
     n_acc_angle_hist = acc_angle_hist/total_time
 
     cos_dt_speed = speed * cos_dt
-    cos_bins = np.hstack((-np.inf, np.linspace(0, 40, 20), np.inf))
+    cos_bins = np.hstack((-np.inf, np.linspace(0, 40, n_bins), np.inf))
     cos_hist, b = np.histogram(cos_dt_speed, bins=cos_bins)
     n_cos_hist = cos_hist/total_time
 
@@ -100,8 +121,8 @@ def acceleration_features(velocity, angle_dt, cos_dt, speed=None):
                      fraction_slow,
                      stops,
                      fraction_stationary])
-    return np.hstack((feats1, n_speed_hist, n_angle_hist, n_acc_hist, n_cos_hist, n_acc_angle_hist))
-    #return np.hstack((feats1, n_speed_hist, n_four_speed_hist, n_acc_hist, n_angle_hist))
+    #return np.hstack((feats1, n_speed_hist, n_angle_hist, n_cos_hist, n_acc_angle_hist))
+    return np.hstack((feats1, n_speed_hist, n_angle_hist, n_acc_hist, n_acc2_hist, n_cos_hist, n_acc_angle_hist))
 
 def main():
     trips = read_trips(sys.argv[1])
